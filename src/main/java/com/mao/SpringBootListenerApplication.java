1fbc66c6bcfbb813a694dad4e7308cdbf93f01ec
markstars <1291324610@qq.com>
2018-09-15 09:47:29 +0800

    Initial commit

---
+++ src/main/java/com/mao/SpringBootListenerApplication.java
@@ -0,0 +1,12 @@
+package com.mao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootListenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootListenerApplication.class, args);
+    }
+}
---
+++ src/main/java/com/mao/listener/HelloApplicationContextInitializer.java
@@ -0,0 +1,11 @@
+package com.mao.listener;
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class HelloApplicationContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+    @Override
+    public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+        System.out.println("ApplicationContextInitializer...Initializer..."+configurableApplicationContext);
+    }
+}
